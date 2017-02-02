99e60705049713e61eecab06968d2dd05126526a
Martin Tasker <martin.tasker@databatix.com>
2017-02-02 09:51:00 +0000

    API: saner exports

--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
 import {createStore} from 'redux';
-
-/*
-  The central point of Redux is that you maintain state, you put state in a store, and apps
-  submit actions to, and subscribe to changes in, that store.  So, we import state from appState,
-  and make it into a store.
-*/
-import actions from './actions';
-
 import appState from './app.state';
 
-var store = createStore(appState);
-
-export default {store, actions};
+// export the actions and the store
+export import actions from './actions';
+export const store = createStore(appState);
